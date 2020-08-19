0f50f56a9b8d12ccdcc4b7ffa5d4883d1f6f280a
Robert Stubbs <sarriss696@gmail.com>
2020-08-19 16:56:45 +0100

    added two new projects, starting with console? is that good, no idea, we will see

--- SaraganGame/main.cpp
+++ SaraganGame/main.cpp
@@ -1,24 +1,24 @@
 #include "GamePCH.h"
 #include "GameLibrary.h"
 
-#include "cCreateWnd.h"
-#include "cRegWnd.h"
-#include "cErrorLogger.h"
-#include "Engine.h"
+//#include "cCreateWnd.h"
+//#include "cRegWnd.h"
+//#include "cErrorLogger.h"
+//#include "Engine.h"
+//
+//#include "RayState.h"
+//#include "MinsweeperState.h"
 
-#include "RayState.h"
-#include "MinsweeperState.h"
-
-Engine* pEngine;
-RayState* raystate;
-MinesweeperState* MSWState;
+//Engine* pEngine;
+//RayState* raystate;
+//MinesweeperState* MSWState;
 
 //https://github.com/HectorPeeters/opengl_premake_boilerplate
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam);
 
 void PreLoad();
-cCreateWnd CreateEngineWindow();
+//cCreateWnd CreateEngineWindow();
 void Load();
 void PostLoad();
 void Update(float dt);
@@ -31,11 +31,11 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLin
 	PreLoad();
 
 	//create the window
-	cCreateWnd Wnd = CreateEngineWindow();
+	//cCreateWnd Wnd = CreateEngineWindow();
 
 	// Initialise Important stuff before window is shown
 	Load();
-	Wnd.show(true);
+	//Wnd.show(true);
 	PostLoad();
 
 	double dt = 0.15;
@@ -60,136 +60,136 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLin
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 {
-	switch (Msg)
-	{
-	case WM_KEYDOWN:
-	{
-		Engine::getEngine().KeyDown(Msg, wParam, lParam);
-	}break;
+	//switch (Msg)
+	//{
+	//case WM_KEYDOWN:
+	//{
+	//	Engine::getEngine().KeyDown(Msg, wParam, lParam);
+	//}break;
 
-	case WM_KEYUP:
-	{
-		Engine::getEngine().KeyUp(Msg, wParam, lParam);
-	}break;
-	case WM_LBUTTONDOWN:
-	{
-	} break;
-	case WM_LBUTTONUP:
-	{
-		Engine::getEngine().MouseUp(Msg, wParam, lParam);
-	} break;
-	case WM_RBUTTONDOWN:
-	{
-		Engine::getEngine().MouseDown(Msg, wParam, lParam);
-	} break;
-	case WM_RBUTTONUP:
-	{
-		Engine::getEngine().MouseUp(Msg, wParam, lParam);
-	} break;
-	case WM_MOUSEMOVE:
-	{
-		Engine::getEngine().MouseMove(Msg, wParam, lParam);
-	} break;
-	case WM_SIZE:
-	{
-		//Resize The OpenGL Window
-		if (wglGetCurrentDC() && wglGetCurrentContext())
-		{
-			Engine::getEngine().ResizeWindow(LOWORD(lParam), HIWORD(lParam));
-		}
-	} break;
-	case WM_CLOSE:
-	{
-		DestroyWindow(hWnd);
-		PostQuitMessage(0);
-	} break;
-	case WM_DESTROY:
-	{
-		DestroyWindow(hWnd);
-		PostQuitMessage(0);
-	} break;
-	default:
-		return DefWindowProc(hWnd, Msg, wParam, lParam);
-	}
+	//case WM_KEYUP:
+	//{
+	//	Engine::getEngine().KeyUp(Msg, wParam, lParam);
+	//}break;
+	//case WM_LBUTTONDOWN:
+	//{
+	//} break;
+	//case WM_LBUTTONUP:
+	//{
+	//	Engine::getEngine().MouseUp(Msg, wParam, lParam);
+	//} break;
+	//case WM_RBUTTONDOWN:
+	//{
+	//	Engine::getEngine().MouseDown(Msg, wParam, lParam);
+	//} break;
+	//case WM_RBUTTONUP:
+	//{
+	//	Engine::getEngine().MouseUp(Msg, wParam, lParam);
+	//} break;
+	//case WM_MOUSEMOVE:
+	//{
+	//	Engine::getEngine().MouseMove(Msg, wParam, lParam);
+	//} break;
+	//case WM_SIZE:
+	//{
+	//	//Resize The OpenGL Window
+	//	if (wglGetCurrentDC() && wglGetCurrentContext())
+	//	{
+	//		Engine::getEngine().ResizeWindow(LOWORD(lParam), HIWORD(lParam));
+	//	}
+	//} break;
+	//case WM_CLOSE:
+	//{
+	//	DestroyWindow(hWnd);
+	//	PostQuitMessage(0);
+	//} break;
+	//case WM_DESTROY:
+	//{
+	//	DestroyWindow(hWnd);
+	//	PostQuitMessage(0);
+	//} break;
+	//default:
+	//	return DefWindowProc(hWnd, Msg, wParam, lParam);
+	//}
 
 	return DefWindowProc(hWnd, Msg, wParam, lParam);
 }
 
 void PreLoad()
 {
-	cErrorLogger::Log().WriteToConsole("#######################################################");
-	cErrorLogger::Log().WriteToConsole("> Created Console");
+	//cErrorLogger::Log().WriteToConsole("#######################################################");
+	//cErrorLogger::Log().WriteToConsole("> Created Console");
 
-	cErrorLogger::Log().WriteToConsole("> Creating Engine");
-	pEngine = new Engine();
+	//cErrorLogger::Log().WriteToConsole("> Creating Engine");
+	//pEngine = new Engine();
 
 }
 
 cCreateWnd CreateEngineWindow()
 {
-	cErrorLogger::Log().WriteToConsole("#######################################################");
-	cErrorLogger::Log().WriteToConsole("> Creating Window");
+	//cErrorLogger::Log().WriteToConsole("#######################################################");
+	//cErrorLogger::Log().WriteToConsole("> Creating Window");
 
-	cRegWnd WinApp(hInst, WndProc, false, 800, 600);
-	WinApp.Register();
+	//cRegWnd WinApp(hInst, WndProc, false, 800, 600);
+	//WinApp.Register();
 
-	cCreateWnd Wnd(&WinApp);
-	hWnd = Wnd.Create();
+	//cCreateWnd Wnd(&WinApp);
+	//hWnd = Wnd.Create();
 
-	cErrorLogger::Log().WriteToConsole("#######################################################");
+	//cErrorLogger::Log().WriteToConsole("#######################################################");
 
-	return Wnd;
+	//return Wnd;
 }
 
 void Load()
 {
-	cErrorLogger::Log().WriteToConsole("> Loading Engine\n");
-	if (!pEngine->init(hWnd, 800, 600)) {
-		cErrorLogger::Log().WriteToConsole("Failed to Initialize OpenGL\n");
-		return;
-	}
+	//cErrorLogger::Log().WriteToConsole("> Loading Engine\n");
+	//if (!pEngine->init(hWnd, 800, 600)) {
+	//	cErrorLogger::Log().WriteToConsole("Failed to Initialize OpenGL\n");
+	//	return;
+	//}
 
 
-	raystate = new RayState();
-	raystate->Init();
+	//raystate = new RayState();
+	//raystate->Init();
 
-	MSWState = new MinesweeperState();
-	MSWState->Init();
+	//MSWState = new MinesweeperState();
+	//MSWState->Init();
 
-	Engine::getEngine().GameFSM->AddState(raystate, false);
-	Engine::getEngine().GameFSM->AddState(MSWState, true);
+	//Engine::getEngine().GameFSM->AddState(raystate, false);
+	//Engine::getEngine().GameFSM->AddState(MSWState, true);
 }
 
 void PostLoad()
 {
-	pEngine->PostInit();
+	//pEngine->PostInit();
 }
 
 void Update(float dt)
 {
-	pEngine->Update(dt);
-	pEngine->UpdateOrth(dt);
+	//pEngine->Update(dt);
+	//pEngine->UpdateOrth(dt);
 }
 
 void Render()
 {
-	pEngine->render();
-	pEngine->renderOrth();
+	//pEngine->render();
+	//pEngine->renderOrth();
 }
 
 void CleanUp()
 {
-	cErrorLogger::Log().WriteToConsole("#######################################################\n");
-	cErrorLogger::Log().WriteToConsole("> Shutting Down\n");
-	cErrorLogger::Log().WriteToConsole("#######################################################\n");
-	cErrorLogger::Log().DestroyConsole();
+	//cErrorLogger::Log().WriteToConsole("#######################################################\n");
+	//cErrorLogger::Log().WriteToConsole("> Shutting Down\n");
+	//cErrorLogger::Log().WriteToConsole("#######################################################\n");
+	//cErrorLogger::Log().DestroyConsole();
 
-	pEngine->cleanup();
+	//pEngine->cleanup();
 
-	if (!UnregisterClass(CLASSNAME, hInst))               // Are We Able To Unregister Class
-	{
-		MessageBox(NULL, L"Could Not Unregister Class.", L"SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
-		hInst = NULL;                         // Set hInstance To NULL
-	}
+	//if (!UnregisterClass(CLASSNAME, hInst))               // Are We Able To Unregister Class
+	//{
+	//	MessageBox(NULL, L"Could Not Unregister Class.", L"SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
+	//	hInst = NULL;                         // Set hInstance To NULL
+	//}
 }
 
--- premake5.lua
+++ premake5.lua
@@ -5,6 +5,115 @@ workspace "Saragan2D"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+project "Game"
+	location "%{prj.name}"
+	kind "ConsoleApp"
+	language "C++"
+	
+	pchheader "GamePCH.h"
+	pchsource "%{prj.name}/GamePCH.cpp"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/**.h",
+		"%{prj.name}/**.cpp",
+		"ThirdParty/glm/include/**.hpp",
+		"ThirdParty/glm/include/**.inl",
+		"ThirdParty/glew/include/**.h",
+		"ThirdParty/freetype-gl/include/**.h"
+	}
+
+	includedirs
+	{
+		"Engine",
+		"ThirdParty/glm/include",
+		"ThirdParty/glew/include",
+		"ThirdParty/freetype2",
+		"ThirdParty/freetype-gl",
+		"ThirdParty/OpenAL/include"
+	}
+
+	links
+	{
+		"Engine"
+	}
+
+	libdirs  
+	{
+		"ThirdParty/glew/lib/**",
+		"ThirdParty/freetype2/lib",
+		"ThirdParty/freetype-gl/lib",
+		"ThirdParty/OpenAL/**"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+		links { "SaraganEngine", "OpenGL32", "glew32", "OpenAL32", "freetype", "freetype-gl" }
+
+	filter "configurations.Debug"
+		defines { "SARAGAN_DEBUG" }
+		symbols "On"
+
+	filter "configurations.Release"
+		defines { "SARAGAN_RELEASE" }
+		optimize "On"
+
+project "Engine"
+	location "%{prj.name}"
+	kind "StaticLib"
+	language "C++"
+
+	pchheader "EnginePCH.h"
+	pchsource "%{prj.name}/EnginePCH.cpp"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+	
+	files {
+		"%{prj.name}/**.h",
+		"%{prj.name}/**.cpp",
+		"ThirdParty/glm/include/**.hpp",
+		"ThirdParty/glm/include/**.inl",
+		"ThirdParty/glew/include/**.h"
+	}
+
+	includedirs
+	{
+		"ThirdParty/glm/include",
+		"ThirdParty/glew/include",
+		"ThirdParty/freetype2",
+		"ThirdParty/freetype-gl",
+		"ThirdParty/OpenAL/include"
+	}
+
+	libdirs  
+	{
+		"ThirdParty/glew/lib/**",
+		"ThirdParty/OpenAL/**"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+
+		postbuildcommands
+		{
+			("{COPY} ../bin/" .. outputdir .. "/%{prj.name} ../bin/" .. outputdir .. "/SaraganGame")
+		}
+
+	filter "configurations.Debug"
+		defines { "SARAGAN_DEBUG" }
+		symbols "On"
+
+	filter "configurations.Release"
+		defines { "SARAGAN_RELEASE" }
+		optimize "On"
+
 project "SaraganGame"
 	location "%{prj.name}"
 	kind "WindowedApp"
@@ -113,7 +222,8 @@ project "SaraganEngine"
 	filter "configurations.Release"
 		defines { "SARAGAN_RELEASE" }
 		optimize "On"
-		
+	
+			
 
 project "freetype-gl"
 	location "ThirdParty/%{prj.name}"
