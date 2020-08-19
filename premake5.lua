3ea01c98a30f0ff574016095e7eca93ec5fa060d
Robert Stubbs <sarriss696@gmail.com>
2020-08-19 17:37:18 +0100

    got glfw working finally...

--- premake5.lua
+++ premake5.lua
@@ -44,18 +44,17 @@ project "Game"
 
 	libdirs  
 	{
-		"ThirdParty/glfw/lib",
 		"ThirdParty/glew/lib/**",
 		"ThirdParty/freetype2/lib",
 		"ThirdParty/freetype-gl/lib",
-		"ThirdParty/OpenAL/**"
+		"ThirdParty/OpenAL/lib"
 	}
 
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
 		staticruntime "on"
-		links { "SaraganEngine", "OpenGL32", "glfw3", "glew32", "OpenAL32", "freetype", "freetype-gl" }
+		links { "SaraganEngine", "OpenGL32", "glfw", "glew32", "OpenAL32", "freetype", "freetype-gl" }
 
 	filter "configurations.Debug"
 		defines { "SARAGAN_DEBUG" }
@@ -76,6 +75,12 @@ project "Engine"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("obj/" .. outputdir .. "/%{prj.name}")
 	
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS",
+		"GLFW_INCLUDE_NONE"
+	}
+
 	files {
 		"%{prj.name}/**.h",
 		"%{prj.name}/**.cpp",
@@ -97,9 +102,8 @@ project "Engine"
 
 	libdirs  
 	{
-		"ThirdParty/glfw/lib",
 		"ThirdParty/glew/lib/**",
-		"ThirdParty/OpenAL/**"
+		"ThirdParty/OpenAL/lib"
 	}
 
 	filter "system:windows"
@@ -277,3 +281,84 @@ project "freetype-gl"
 	filter "configurations.Release"
 		defines { "SARAGAN_RELEASE" }
 		optimize "On"
+
+project "GLFW"
+	location "ThirdParty/%{prj.name}"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"ThirdParty/%{prj.name}/include/GLFW/glfw3.h",
+		"ThirdParty/%{prj.name}/include/GLFW/glfw3native.h",
+		"ThirdParty/%{prj.name}/src/glfw_config.h",
+		"ThirdParty/%{prj.name}/src/context.c",
+		"ThirdParty/%{prj.name}/src/init.c",
+		"ThirdParty/%{prj.name}/src/input.c",
+		"ThirdParty/%{prj.name}/src/monitor.c",
+		"ThirdParty/%{prj.name}/src/vulkan.c",
+		"ThirdParty/%{prj.name}/src/window.c"
+	}
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+		staticruntime "On"
+
+		files
+		{
+			"ThirdParty/%{prj.name}/src/x11_init.c",
+			"ThirdParty/%{prj.name}/src/x11_monitor.c",
+			"ThirdParty/%{prj.name}/src/x11_window.c",
+			"ThirdParty/%{prj.name}/src/xkb_unicode.c",
+			"ThirdParty/%{prj.name}/src/posix_time.c",
+			"ThirdParty/%{prj.name}/src/posix_thread.c",
+			"ThirdParty/%{prj.name}/src/glx_context.c",
+			"ThirdParty/%{prj.name}/src/egl_context.c",
+			"ThirdParty/%{prj.name}/src/osmesa_context.c",
+			"ThirdParty/%{prj.name}/src/linux_joystick.c"
+		}
+
+		defines
+		{
+			"_GLFW_X11"
+		}
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+
+		files
+		{
+			"ThirdParty/%{prj.name}/src/win32_init.c",
+			"ThirdParty/%{prj.name}/src/win32_joystick.c",
+			"ThirdParty/%{prj.name}/src/win32_monitor.c",
+			"ThirdParty/%{prj.name}/src/win32_time.c",
+			"ThirdParty/%{prj.name}/src/win32_thread.c",
+			"ThirdParty/%{prj.name}/src/win32_window.c",
+			"ThirdParty/%{prj.name}/src/wgl_context.c",
+			"ThirdParty/%{prj.name}/src/egl_context.c",
+			"ThirdParty/%{prj.name}/src/osmesa_context.c"
+		}
+
+		defines 
+		{ 
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+		
+		postbuildcommands
+		{
+			("{COPY} ../../bin/" .. outputdir .. "/%{prj.name} ../../bin/" .. outputdir .. "/SaraganGame")
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
