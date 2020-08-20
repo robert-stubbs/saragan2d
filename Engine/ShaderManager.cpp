d0f6ddecd7de5ac2be7fc4be51d1b2f2914bab5e
robert_stubbs <sarriss696@gmail.com>
2020-08-20 18:04:04 +0100

    updated shaders

---
+++ Engine/ShaderManager.cpp
@@ -0,0 +1,38 @@
+#include "EnginePCH.h"
+#include "ShaderManager.h"
+
+
+namespace GameEngine {
+
+	ShaderManager::ShaderManager()
+	{
+		engine_type = RenderEngines::None;
+		shader_engine = nullptr;
+	}
+
+	ShaderManager::ShaderManager(RenderEngines EngineType)
+	{
+		engine_type = EngineType;
+		shader_engine = nullptr;
+
+		//switch (engine_type)
+		//{
+		//case GameEngine::None:
+		//	render_engine = nullptr;
+		//	return;
+		//	break;
+		//case GameEngine::OpenGL:
+		//	render_engine = std::make_shared<OpenGLRenderEngine>();
+		//	break;
+		//case GameEngine::DirectX:
+		//	break;
+		//default:
+		//	break;
+		//}
+	}
+
+	ShaderManager::~ShaderManager()
+	{
+
+	}
+}
---
+++ Engine/ShaderManager.h
@@ -0,0 +1,27 @@
+#ifndef _SHADER_MANAGER_
+#define _SHADER_MANAGER_
+
+#include "Types.h"
+#include "RenderEngineBase.h"
+#include "Shader.h"
+#include "Renderer.h"
+
+namespace GameEngine {
+
+	class ShaderManager
+	{
+		private:
+			RenderEngines engine_type;
+			std::shared_ptr<Shader> shader_engine;
+		public:
+
+			ShaderManager();
+			ShaderManager(RenderEngines EngineType);
+			virtual ~ShaderManager();
+
+			Shader& RenderEngine() { return *shader_engine; }
+	};
+
+}
+
+#endif
