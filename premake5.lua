a44e032cac31d378250c72b989c009794100f13d
robert_stubbs <sarriss696@gmail.com>
2020-09-05 10:58:59 +0100

    added IMGUI library

--- premake5.lua
+++ premake5.lua
@@ -24,8 +24,7 @@ project "Game"
 		"ThirdParty/glew/include/**.h",
 		"ThirdParty/glfw/include/**.h",
 		"ThirdParty/freetype-gl/include/**.h",
-		"ThirdParty/ImGui/*.h",
-		"ThirdParty/ImGui/*.cpp"
+		"ThirdParty/ImGui/*.h"
 	}
 
 	includedirs
@@ -80,7 +79,6 @@ project "Engine"
 	
 	defines
 	{
-		"_CRT_SECURE_NO_WARNINGS",
 		"GLFW_INCLUDE_NONE"
 	}
 
@@ -91,8 +89,7 @@ project "Engine"
 		"ThirdParty/glm/include/**.inl",
 		"ThirdParty/glfw/include/**.h",
 		"ThirdParty/glew/include/**.h",
-		"ThirdParty/ImGui/*.h",
-		"ThirdParty/ImGui/*.cpp"
+		"ThirdParty/ImGui/*.h"
 	}
 
 	includedirs
@@ -128,118 +125,7 @@ project "Engine"
 
 	filter "configurations.Release"
 		defines { "SARAGAN_RELEASE" }
-		optimize "On"
-
-project "SaraganGame"
-	location "%{prj.name}"
-	kind "WindowedApp"
-	language "C++"
-	
-	pchheader "GamePCH.h"
-	pchsource "%{prj.name}/GamePCH.cpp"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("obj/" .. outputdir .. "/%{prj.name}")
-
-	files {
-		"%{prj.name}/**.h",
-		"%{prj.name}/**.cpp",
-		"ThirdParty/glm/include/**.hpp",
-		"ThirdParty/glm/include/**.inl",
-		"ThirdParty/glew/include/**.h",
-		"ThirdParty/freetype-gl/include/**.h"
-	}
-
-	includedirs
-	{
-		"SaraganEngine",
-		"ThirdParty/glm/include",
-		"ThirdParty/glew/include",
-		"ThirdParty/freetype2",
-		"ThirdParty/freetype-gl",
-		"ThirdParty/OpenAL/include"
-	}
-
-	links
-	{
-		"SaraganEngine"
-	}
-
-	libdirs  
-	{
-		"ThirdParty/glew/lib/**",
-		"ThirdParty/freetype2/lib",
-		"ThirdParty/freetype-gl/lib",
-		"ThirdParty/OpenAL/**"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "on"
-		links { "SaraganEngine", "OpenGL32", "glew32", "OpenAL32", "freetype", "freetype-gl" }
-
-	filter "configurations.Debug"
-		defines { "SARAGAN_DEBUG" }
-		symbols "On"
-
-	filter "configurations.Release"
-		defines { "SARAGAN_RELEASE" }
-		optimize "On"
-
-project "SaraganEngine"
-	location "%{prj.name}"
-	kind "StaticLib"
-	language "C++"
-
-	pchheader "EnginePCH.h"
-	pchsource "%{prj.name}/EnginePCH.cpp"
-	
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("obj/" .. outputdir .. "/%{prj.name}")
-	
-	files {
-		"%{prj.name}/**.h",
-		"%{prj.name}/**.cpp",
-		"ThirdParty/glm/include/**.hpp",
-		"ThirdParty/glm/include/**.inl",
-		"ThirdParty/glew/include/**.h"
-	}
-
-	includedirs
-	{
-		"ThirdParty/glm/include",
-		"ThirdParty/glew/include",
-		"ThirdParty/freetype2",
-		"ThirdParty/freetype-gl",
-		"ThirdParty/OpenAL/include"
-	}
-
-	libdirs  
-	{
-		"ThirdParty/glew/lib/**",
-		"ThirdParty/OpenAL/**"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "on"
-
-		postbuildcommands
-		{
-			("{COPY} ../bin/" .. outputdir .. "/%{prj.name} ../bin/" .. outputdir .. "/SaraganGame")
-		}
-
-	filter "configurations.Debug"
-		defines { "SARAGAN_DEBUG" }
-		symbols "On"
-
-	filter "configurations.Release"
-		defines { "SARAGAN_RELEASE" }
-		optimize "On"
-	
-			
+		optimize "On"	
 
 project "freetype-gl"
 	location "ThirdParty/%{prj.name}"
