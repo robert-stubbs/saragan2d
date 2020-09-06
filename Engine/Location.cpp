564dc84195e927550e2c4f9bf0da8e08bd6d3129
robert_stubbs <sarriss696@gmail.com>
2020-09-06 11:06:38 +0100

    Added Location/Position component

---
+++ Engine/Location.cpp
@@ -0,0 +1,53 @@
+#include "EnginePCH.h"
+#include "Location.h"
+
+#include "cErrorLogger.h"
+
+namespace GameEngine {
+
+	Location::Location()
+	{
+		m_type = "Location";
+		x = 0.0f;
+		y = 0.0f;
+		z = 0.0f;
+	}
+
+	Location::~Location()
+	{
+
+	}
+
+	bool Location::Init()
+	{
+		return true;
+	}
+
+	void Location::Update(float dt)
+	{
+
+	}
+
+	glm::vec3 Location::getPosition()
+	{
+		return glm::vec3({ x, y, x });
+	}
+
+	glm::mat4 Location::getPositionMat()
+	{
+		return glm::translate(glm::mat4(1.0f), glm::vec3(x, y, z));
+	}
+
+	void Location::setPosition(float m_x, float m_y, float m_z)
+	{
+		x = m_x;
+		y = m_y;
+		z = m_z;
+	}
+
+	void Location::CleanUp()
+	{
+
+	}
+
+}
---
+++ Engine/Location.h
@@ -0,0 +1,37 @@
+#ifndef _SOUND_H_
+#define _SOUND_H_
+
+#include "Types.h"
+#include "Component.h"
+
+namespace GameEngine {
+
+	class Location : public Component {
+	private:
+		float x;
+		float y;
+		float z;
+
+
+
+	public:
+		Location();
+		virtual ~Location();
+
+		virtual bool Init() override;
+
+		virtual void Update(float dt) override;
+
+		virtual void Render() {};
+
+		virtual void CleanUp() override;
+
+		virtual glm::vec3 getPosition();
+		virtual glm::mat4 getPositionMat();
+		virtual void setPosition(float m_x, float m_y, float m_z);
+
+	};
+
+}
+
+#endif
