8a5e5b0a60027ceccbfde511e59af20ba653ed02
Robert Stubbs <sarriss696@gmail.com>
2020-10-18 16:43:25 +0100

    ImGui Docking and viewports

--- Engine/OpenGLGUIPlatform.cpp
+++ Engine/OpenGLGUIPlatform.cpp
@@ -4,8 +4,9 @@
 #include "Engine.h"
 
 #define IMGUI_IMPL_OPENGL_LOADER_GLEW
+
 #include "backends/imgui_impl_opengl3.cpp"
-#include "examples/imgui_impl_glfw.cpp"
+#include "backends/imgui_impl_glfw.cpp"
 
 namespace GameEngine
 {
