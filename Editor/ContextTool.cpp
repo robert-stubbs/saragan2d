f580817d66b4a2bdffb78728f3a4982efa951b8f
Robert Stubbs <sarriss696@gmail.com>
2020-10-20 08:56:48 +0100

    working through docking

--- Editor/ContextTool.cpp
+++ Editor/ContextTool.cpp
@@ -12,7 +12,13 @@ namespace Editor {
 		if (GUI::GetGUI().HasInstance()) {
 			GUI::Get().Begin("Main");
 
-				ImGui::TextUnformatted("Main Area Test");
+				ImVec2 pos = ImGui::GetCursorScreenPos();
+
+				//ImGui::GetWindowDrawList()->AddImage(
+				//	(void*)window.getRenderTexture(), ImVec2(ImGui::GetCursorScreenPos()),
+				//	ImVec2(ImGui::GetCursorScreenPos().x + window.getWidth() / 2, ImGui::GetCursorScreenPos().y + window.getHeight() / 2), ImVec2(0, 1), ImVec2(1, 0));
+
+
 
 			GUI::Get().End();
 		}
